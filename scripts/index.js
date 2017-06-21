07c123fa0acaf0c3e54dfc3a1a5b8233fb61aa10
Zamith <zamith.28@gmail.com>
2017-06-21 17:24:58 -0400

    Uses ngrok tunnel

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       "X-User-Token": "67yYVvyKVxALu2N8mXVS"
     },
     dataType: "json",
-    url: "http://app.lvh.me:5000/api/interstitials/musical"
+    url: "http://c9f9006f.ngrok.io/api/interstitials/musical"
   }).done(function(data) {
     $(".ProviderInfo").html(data.html);
     $("head").append("<link rel='stylesheet' href='"+data.stylesheet+"' type='text/css'>");
