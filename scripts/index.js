632aba17d1de4efd746a3eb8311d060f9055e513
Zamith <zamith.28@gmail.com>
2017-06-22 11:19:05 -0400

    Use new api

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
       "X-User-Token": "67yYVvyKVxALu2N8mXVS"
     },
     dataType: "json",
-    url: "http://c9f9006f.ngrok.io/api/interstitials/musical"
+    url: "http://e9ad097a.ngrok.io/api/interstitials/musical"
   }).done(function(data) {
-    $(".ProviderInfo").html(data.html);
+    $("#interstitial").html(data.html);
     $("head").append("<link rel='stylesheet' href='"+data.stylesheet+"' type='text/css'>");
   });
 });
